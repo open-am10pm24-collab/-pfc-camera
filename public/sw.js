6a90158d53af02004bcd7ff572e005eb2f13e45c
open-am10pm24-collab <open-am10pm24@i.softbank.jp>
2026-08-21 22:41:45 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pfc-v2-2';
+const CACHE='pfc-v2-3';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.json'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
